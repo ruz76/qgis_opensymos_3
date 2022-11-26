640fac88648ee1bf3febc41ccf09a8988c4f6f9b
tomas3t5 <jasnickyt@gmail.com>
2022-11-26 23:34:07 +0100

    ver6

--- jaster_polluted_source_preparation.py
+++ jaster_polluted_source_preparation.py
@@ -139,7 +139,7 @@ class Formular(QDialog, FORM_CLASS):
         cellsize = 0.0001 #Cell Size in WGS 84 will be 10 x 10 meters
         crs = "EPSG:4326" #WGS 84 System
         # input = processing.getObject(newlayer.name()) #Use the processing.getObject to get information from our vector layer
-        input = QgsProcessingUtils.mapLayerFromString(newlayer) #Use the processing.getObject to get information from our vector layer
+        input = QgsProcessingUtils.mapLayerFromString(str(newlayer)) #Use the processing.getObject to get information from our vector layer
         xmin = (input.textent().xMinimum()) #extract the minimum x coord from our layer
         xmax = (input.extent().xMaximum()) #extract our maximum x coord from our layer
         ymin = (input.extent().yMinimum()) #extract our minimum y coord from our layer
