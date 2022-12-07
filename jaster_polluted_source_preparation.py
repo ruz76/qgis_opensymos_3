f79909037dd45dcbf3b0036e2d8e03c063180f41
tomas3t5 <jasnickyt@gmail.com>
2022-12-07 14:01:26 +0100

    ver8

--- jaster_polluted_source_preparation.py
+++ jaster_polluted_source_preparation.py
@@ -151,16 +151,16 @@ class Formular(QDialog, FORM_CLASS):
             polygon_extent = str(xmin)+ ',' + str(xmax)+ ',' +str(ymin)+ ',' +str(ymax)
             create_rastr = processing.run("gdal:rasterize", {'INPUT': layer,'FIELD':'Id','BURN':0,'USE_Z':False,'UNITS':1,'WIDTH':cell_size,'HEIGHT':cell_size,
                                           'EXTENT':polygon_extent,'NODATA':0,'OPTIONS':'','DATA_TYPE':5,'INIT':None,'INVERT':False,'EXTRA':'','OUTPUT':'TEMPORARY_OUTPUT'})
-        rastr = QgsRasterLayer(create_rastr['OUTPUT'])
+            rastr = QgsRasterLayer(create_rastr['OUTPUT'])
             #rastr.setName('polygon_raster')
-        QgsMessageLog.logMessage("Rastrový grid je hotový.", "Messages")
-        QgsProject.instance().addMapLayer(rastr)
+            QgsMessageLog.logMessage("Rastrový grid je hotový.", "Messages")
+            QgsProject.instance().addMapLayer(rastr)
 
-        create_centroids = processing.run("native:pixelstopoints", {'INPUT_RASTER':rastr,'RASTER_BAND':1,'FIELD_NAME':'VALUE','OUTPUT':'TEMPORARY_OUTPUT'})
-        centroids = create_centroids['OUTPUT']
-        centroids.setName('polygon_centroids')
-        QgsMessageLog.logMessage("Centroidy sú hotové.", "Messages")
-        QgsProject.instance().addMapLayer(centroids)
+            create_centroids = processing.run("native:pixelstopoints", {'INPUT_RASTER':rastr,'RASTER_BAND':1,'FIELD_NAME':'VALUE','OUTPUT':'TEMPORARY_OUTPUT'})
+            centroids = create_centroids['OUTPUT']
+            centroids.setName('polygon_centroids')
+            QgsMessageLog.logMessage("Centroidy sú hotové.", "Messages")
+            QgsProject.instance().addMapLayer(centroids)
 
         create_count = processing.run("native:countpointsinpolygon", {'POLYGONS': layer,'POINTS': centroids,'WEIGHT':'','CLASSFIELD':'','FIELD':'NUMPOINTS','OUTPUT':'TEMPORARY_OUTPUT'})
         count = create_count['OUTPUT']
