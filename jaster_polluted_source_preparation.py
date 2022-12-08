5c23261256456f555a114fc1112d0afb93f70dcb
tomas3t5 <jasnickyt@gmail.com>
2022-12-08 23:22:34 +0100

    ver9

--- jaster_polluted_source_preparation.py
+++ jaster_polluted_source_preparation.py
@@ -134,7 +134,8 @@ class Formular(QDialog, FORM_CLASS):
         #QgsVectorFileWriter("emise.shp", vystup, fields, "UTF-8", finalgrid.crs(), "ESRI Shapefile")
         #QgsVectorFileWriter.writeAsVectorFormat(finalgrid, "emise.shp", "UTF-8")
 
-        QgsVectorFileWriter.writeAsVectorFormat(finalgrid, vystup,"utf-8",None,"ESRI Shapefile")
+        #QgsVectorFileWriter.writeAsVectorFormat(finalgrid, vystup,"utf-8",None,"ESRI Shapefile")
+        QgsVectorFileWriter.writeAsVectorFormat(finalgrid, vystup,"utf-8","ESRI Shapefile")
         # with open(Output, mode='w', encoding='utf-8') as soubor:
         #     print("<p> " + str(area["Id"]) + " - " + " <img src=area_" + str(area["Id"]) + ".png width=300/></p>\n", file=soubor)
         # QgsMessageLog.logMessage("Výsledek byl uložen do: " + str(Output), "Messages")
